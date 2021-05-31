57c04db43286eb805b9309510cfcfaf00b240a68
tekenoke <keiichi-nm@hi-ho.ne.jp>
2021-05-31 17:53:39 +0900

    公開用

---
+++ String2Fontdata/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace String2Fontdata
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.btnFileLoad = new System.Windows.Forms.Button();
+			this.btnFsave = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.tbxChHeight = new System.Windows.Forms.TextBox();
+			this.tbxChWidth = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.tbxFname = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.cbxTFTgraphic = new System.Windows.Forms.CheckBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.White;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 130);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(384, 192);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// btnFileLoad
+			// 
+			this.btnFileLoad.Location = new System.Drawing.Point(18, 20);
+			this.btnFileLoad.Name = "btnFileLoad";
+			this.btnFileLoad.Size = new System.Drawing.Size(138, 92);
+			this.btnFileLoad.TabIndex = 1;
+			this.btnFileLoad.Text = "FileRead\r\n&&\r\nConvert";
+			this.btnFileLoad.UseVisualStyleBackColor = true;
+			this.btnFileLoad.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// btnFsave
+			// 
+			this.btnFsave.Enabled = false;
+			this.btnFsave.Location = new System.Drawing.Point(628, 20);
+			this.btnFsave.Name = "btnFsave";
+			this.btnFsave.Size = new System.Drawing.Size(138, 92);
+			this.btnFsave.TabIndex = 3;
+			this.btnFsave.Text = "Textファイル\r\n書出し\r\n(上書き確認なし)";
+			this.btnFsave.UseVisualStyleBackColor = true;
+			this.btnFsave.Click += new System.EventHandler(this.btnConvert_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.Filter = "bmp|*.bmp|png|*.png";
+			// 
+			// tbxChHeight
+			// 
+			this.tbxChHeight.Location = new System.Drawing.Point(80, 26);
+			this.tbxChHeight.Name = "tbxChHeight";
+			this.tbxChHeight.Size = new System.Drawing.Size(69, 25);
+			this.tbxChHeight.TabIndex = 4;
+			this.tbxChHeight.Text = "---";
+			// 
+			// tbxChWidth
+			// 
+			this.tbxChWidth.Location = new System.Drawing.Point(80, 63);
+			this.tbxChWidth.Name = "tbxChWidth";
+			this.tbxChWidth.Size = new System.Drawing.Size(69, 25);
+			this.tbxChWidth.TabIndex = 5;
+			this.tbxChWidth.Text = "---";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 66);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(49, 18);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "Width";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 27);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(56, 18);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "Height";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("ＭＳ 明朝", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBox1.Location = new System.Drawing.Point(3, 612);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBox1.Size = new System.Drawing.Size(754, 444);
+			this.textBox1.TabIndex = 4;
+			this.textBox1.TabStop = false;
+			this.textBox1.WordWrap = false;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.tbxChHeight);
+			this.groupBox1.Controls.Add(this.tbxChWidth);
+			this.groupBox1.Location = new System.Drawing.Point(172, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(168, 100);
+			this.groupBox1.TabIndex = 9;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "キャラクタサイズ";
+			// 
+			// saveFileDialog1
+			// 
+			this.saveFileDialog1.Filter = "txt|*.txt|bmp|*.bmp";
+			// 
+			// tbxFname
+			// 
+			this.tbxFname.Location = new System.Drawing.Point(362, 39);
+			this.tbxFname.Name = "tbxFname";
+			this.tbxFname.Size = new System.Drawing.Size(241, 25);
+			this.tbxFname.TabIndex = 10;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(358, 16);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(126, 18);
+			this.label3.TabIndex = 11;
+			this.label3.Text = "書出しファイル名";
+			// 
+			// cbxTFTgraphic
+			// 
+			this.cbxTFTgraphic.AutoSize = true;
+			this.cbxTFTgraphic.Checked = true;
+			this.cbxTFTgraphic.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.cbxTFTgraphic.Location = new System.Drawing.Point(362, 78);
+			this.cbxTFTgraphic.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+			this.cbxTFTgraphic.Name = "cbxTFTgraphic";
+			this.cbxTFTgraphic.Size = new System.Drawing.Size(117, 22);
+			this.cbxTFTgraphic.TabIndex = 12;
+			this.cbxTFTgraphic.Text = "UniGraphic";
+			this.cbxTFTgraphic.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(930, 1060);
+			this.Controls.Add(this.cbxTFTgraphic);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.tbxFname);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.btnFsave);
+			this.Controls.Add(this.btnFileLoad);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "Form1";
+			this.Text = "ビットマップーキャラクタコード変換";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button btnFileLoad;
+		private System.Windows.Forms.Button btnFsave;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.TextBox tbxChHeight;
+		private System.Windows.Forms.TextBox tbxChWidth;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.TextBox tbxFname;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.CheckBox cbxTFTgraphic;
+	}
+}
+
---
+++ String2Fontdata/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+// ビットマップをフォントデータに変換する
+// Ver1.0 2021/05/31	public版
+
+namespace String2Fontdata
+{
+	public partial class Form1 : Form
+	{
+		const int Hcount = 6, Wcount = 16;
+		int charW, charH;
+		String fpath;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		// *** file読込&変換button ***
+ 		private void button1_Click(object sender, EventArgs e) { 
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				string fname = openFileDialog1.FileName;	// 読込ファイル名を.txt保存用にtextboxに保持しておく
+				tbxFname.Text = Path.GetFileNameWithoutExtension(openFileDialog1.FileName) + ".txt";
+				tbxFname.Refresh();
+				btnFsave.Enabled = true;
+				fpath = Path.GetDirectoryName(openFileDialog1.FileName);
+				Image img = Image.FromFile(fname);
+				pictureBox1.Image=img;
+				charH = pictureBox1.Height / Hcount;
+				charW = pictureBox1.Width / Wcount;
+				tbxChHeight.Text = charH.ToString();
+				tbxChWidth.Text = charW.ToString();
+				Bitmap bmp = new Bitmap(pictureBox1.Image);
+				FormationOfTwoValues(ref bmp, 0.5f);	// 安全のため２値化する
+				pictureBox1.Image = bmp;
+				pictureBox1.Refresh();
+				bmp2chcode(ref bmp);	// code変換
+			}
+		}
+
+		// *** キャラクタコード変換 ***
+		private void bmp2chcode(ref Bitmap bmp) {
+			int charHcount = charH / 8; // 縦のバイト数
+			String strg;
+
+			textBox1.Clear();
+			if (!cbxTFTgraphic.Checked) {	// AQM LCD表示用
+				strg = "const\tunsigned char\t" + Path.GetFileNameWithoutExtension(tbxFname.Text) + 
+					"[96][" + charHcount.ToString() + "][" + charW.ToString() + "] = {\r\n";
+				textBox1.AppendText(strg);
+				for (int ivt = 0; ivt < Hcount; ivt++) {   // 文字数縦カウント(縦48bitの場合0-5)
+					for (int ihl = 0; ihl < Wcount; ihl++) {    // 文字数横カウント0-15
+						strg = "\t{\r\n";
+						for (int iyp = 0; iyp < charHcount; iyp++) {   // 1文字縦バイトカウント
+							strg += "\t\t{";
+							for (int ixp = 0; ixp < charW; ixp++) {   // 1文字横ビットカウント
+								int bpat = 0x1, mpat = 0x0;
+								for (int bcnt = 0; bcnt < 8; bcnt++) {     // 1文字内のビットカウント
+									Color color = bmp.GetPixel(ihl * charW + ixp, ivt * charH + iyp * 8 + bcnt);
+									float fTemp = color.GetBrightness();
+									if (fTemp < 0.5f)
+									{
+										mpat |= bpat;
+									}
+									bpat <<= 1;
+								}
+								strg += "0x" + mpat.ToString("X2");
+								if ((ixp != (charW - 1) && ivt != (charH - 1))) strg += ", ";
+							}
+							strg += "}";
+							if (iyp != (charHcount - 1)) {
+								strg += ",";
+							}
+							strg += "\r\n";
+						}
+						strg += "\t}";
+						if ((ivt != (Hcount - 1)) || (ihl != (Wcount - 1))) {	// 最終data以外は','で区切る
+							strg += ",";
+						}
+						strg += "\r\n";
+						textBox1.AppendText(strg);
+					}
+				}
+			} else {	// UniGraphicの場合、文字数,縦バイト数,横ビット数,縦バイト数のヘッダを付ける
+				strg = "const\tunsigned char\t"+ Path.GetFileNameWithoutExtension(tbxFname.Text) + 
+					"[] __attribute__((aligned (2))) = {\r\n\t" + (charH / 8 * charW + 1).ToString() + ", " + charW.ToString() + ", " +
+					charH.ToString() + ", " + (charH / 8).ToString() + ", \r\n";
+				textBox1.AppendText(strg);
+				int iyp;
+				for (int ivt = 0; ivt < Hcount; ivt++) {    // 文字数縦カウント(縦48bitの場合0-5)
+					for (int ihl = 0; ihl < Wcount; ihl++) {    // 文字数横カウント0-15
+						int ixp;	// プロポーショナルヘッダ計算
+						for (ixp = 0; ixp < charW; ixp++) {
+							if (bmp.GetPixel((ihl * charW + ixp), ivt * charH).GetBrightness() < 0.5f) {	// いちばん上のビットが黒の場合はそこで終了
+								bmp.SetPixel((ihl * charW + ixp), ivt * charH, Color.White); // そのビットはとりあえず白で埋めておく
+								break;
+							}
+						}
+						strg = "\t0x" + ixp.ToString("X2") + ",";
+						for (int ixp2 = 0; ixp2 < charW; ixp2++) {   // 1文字横ビットカウント
+							for (iyp = 0; iyp < charHcount; iyp++) {   // 1文字縦バイトカウント
+								int bpat = 0x1, mpat = 0x0;
+								for (int bcnt = 0; bcnt < 8; bcnt++) {     // 1文字内のビットカウント
+									Color color = bmp.GetPixel(ihl * charW + ixp2, ivt * charH + iyp * 8 + bcnt);
+									float fTemp = color.GetBrightness();
+									if (fTemp < 0.5f) {
+										mpat |= bpat;
+									}
+									bpat <<= 1;
+								}
+								strg += "0x" + mpat.ToString("X2");
+								if (iyp != (charHcount - 1)) {
+									strg += ",";
+								}
+							}
+							if ((ivt != (Hcount - 1)) || (ihl != (Wcount - 1)) || (ixp2 != (charW-1))) {
+								strg += ",";
+							}
+						}
+						strg += "\r\n";
+						textBox1.AppendText(strg);
+					}
+				}
+			}
+			textBox1.AppendText("};\r\n");
+		}
+
+		// *** ２値化(th:0.0～1.0)*** 
+		private void FormationOfTwoValues(ref Bitmap bmp, float th) {
+			Color color;
+
+			for (Int32 y = 0; y < bmp.Size.Height; y++) {
+				for (Int32 x = 0; x < bmp.Size.Width; x++) {
+					color = bmp.GetPixel(x, y);
+					float fTemp = color.GetBrightness();
+					if (fTemp < th) {
+						bmp.SetPixel(x, y, Color.Black);
+					} else {
+						bmp.SetPixel(x, y, Color.White);
+					}
+				}
+			}
+			return;
+		}
+
+		private void btnConvert_Click(object sender, EventArgs e) {
+			FileStream fs = new FileStream(fpath+"\\"+tbxFname.Text, FileMode.Create);
+			StreamWriter sw = new StreamWriter(fs, Encoding.GetEncoding("shift_jis"));
+			sw.Write(textBox1.Text);
+			sw.Close();
+			fs.Close();
+		}
+	}
+}
